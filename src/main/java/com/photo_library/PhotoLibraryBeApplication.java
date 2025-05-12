b381c204059468172442f2ef8d3f09ccee27c106
GiorgioEmanuele <g.capuno@outlook.it>
2025-05-12 11:42:39 +0200

    spring boot project structure

---
+++ src/main/java/com/photo_library/PhotoLibraryBeApplication.java
@@ -0,0 +1,13 @@
+package com.photo_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoLibraryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoLibraryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photo_library/PhotoLibraryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoLibraryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
